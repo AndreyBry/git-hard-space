862799469739a737f8c31db4fbe9efb8df36d0c9
Rokymiel <33631970+rokymiel@users.noreply.github.com>
2023-05-29 00:21:48 +0300

    Real fall point method prototype added
    Signed-off-by: Rokymiel <33631970+rokymiel@users.noreply.github.com>

--- launch.sh
+++ launch.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+calculate_fall_point() {
+  let POINT=1
+  let POINT=POINT*2
+  let POINT=POINT+3
+  
+  echo "$POINT" > fp.txt
+}
+
 calculate_accident_fall_point() {
   echo "0" > accident_fp.txt
 }
@@ -16,6 +24,7 @@ calculate_fuel() {
   echo "0" > fuel.txt
 }
 
+calculate_fall_point
 calculate_accident_fall_point
 calculate_satellite_orbit
 calculate_flight_time
